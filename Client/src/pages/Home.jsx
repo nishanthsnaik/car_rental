c565efc91977f284307f179ee538048d92d3f063
Nishanth S Naik <nishanthsnaik35.com>
2026-05-16 07:59:17 +0530

    Fix newsletter import casing

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero'
 import FeatureSelection from '../components/FeatureSelection'
 import Banner from '../components/Banner'
 import Testimonial from '../components/Testimonial'
-import NewsLetter from '../components/NewsLetter'
+import NewsLetter from '../components/Newsletter'
 import { motion } from 'motion/react'
 
 const Home = () => {
